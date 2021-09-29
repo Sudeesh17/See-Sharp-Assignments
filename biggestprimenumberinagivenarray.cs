1cc10d672a4f3d0207d0e44fb4b286d55fe65643
sudeesh <sudeeshpuli143@gmail.com>
2021-09-29 22:38:38 +0530

    first commit

---
+++ biggestprimenumberinagivenarray.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignmentfiles
+{
+    class Class1
+    {
+        static void prime(int[] arr, int n)
+        {
+
+            int max_val = arr.Max();
+
+            List<bool> prime = new List<bool>();
+            for (int i = 0; i < max_val + 1; i++)
+                prime.Add(true);
+
+
+            prime.Insert(0, false);
+            prime.Insert(1, false);
+            for (int p = 2; p * p <= max_val; p++)
+            {
+
+
+                if (prime[p] == true)
+                {
+
+
+                    for (int i = p * 2; i <= max_val; i += p)
+                        prime.Insert(i, false);
+                }
+            }
+
+
+            int minimum = int.MaxValue;
+            int maximum = int.MinValue;
+            for (int i = 0; i < n; i++)
+                if (prime[arr[i]])
+                {
+                    minimum = Math.Min(minimum, arr[i]);
+                    maximum = Math.Max(maximum, arr[i]);
+                }
+
+            Console.WriteLine("Minimum : " + minimum);
+            Console.WriteLine("Maximum : " + maximum);
+        }
+
+
+        public static void Main()
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+            int n = arr.Length;
+
+            prime(arr, n);
+        }
+    }
+}
---
+++ workingdayorholiday.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignmentfiles
+{
+    class holidays
+    {
+
+
+        static void Main(string[] args)
+        {
+            int weekday;
+
+
+            Console.Write("Enter weekday number (1-7): ");
+            weekday = Convert.ToInt32(Console.ReadLine());
+
+
+            switch (weekday)
+            {
+                case 1:
+                    Console.WriteLine("It is MONDAY  and it's working day");
+                    break;
+                case 2:
+                    Console.WriteLine("It is TUESDAY it's working day");
+                    break;
+                case 3:
+                    Console.WriteLine("It is WEDNESDAY it's working day");
+                    break;
+                case 4:
+                    Console.WriteLine("It is THURSDAY it's working day");
+                    break;
+                case 5:
+                    Console.WriteLine("It is FRIDAY it's working day");
+                    break;
+                case 6:
+                    Console.WriteLine("It is SATURDAY and it's Holiday");
+                    break;
+                case 7:
+                    Console.WriteLine("It is SUNDAY and it's Holiday");
+                    break;
+
+            }
+
+
+        }
+    }
+}
